36df46335c45d53cf6136d59f906ef953d119797
CenturyChen <centurychen1103@gmail.com>
2025-10-19 19:48:16 +0800

    Delete MCP-main directory

--- MCP-main/scripts/run_ood_benchmark_rn50.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-CUDA_VISIBLE_DEVICES=0 python mcp_runner.py     --config configs \
-                                                --datasets I/A/V/R/S \
-                                                --backbone RN50 --res False
-                                                
-                                              
--- MCP-main/scripts/run_ood_benchmark_vit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-CUDA_VISIBLE_DEVICES=0 python mcp_runner.py     --config configs \
-                                                --datasets I/A/V/R/S \
-                                                --backbone ViT-B/16 --res False
-                            
